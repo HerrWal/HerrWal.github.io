634a06ef22d723de4463a458f6bad70e9b4d4034
HerrWal <85037202+HerrWal@users.noreply.github.com>
2022-12-07 22:01:45 -0500

    day 2 project
    Added route and error handlers

--- app.js
+++ app.js
@@ -1,6 +1,52 @@
 const express = require('express');
 const app = express();
 const data = require('./data.json');
+const { projects} = data; 
 // the path module which can be used when setting the absolute path in the express.static function.
+const path = require('path');
 
+app.set('view engine', 'pug');
 
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+/* Routes */
+
+// index route
+app.get('/', (req, res) => {
+    res.render('index', { projects });
+});
+
+// about route
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/projects/:id', (req, res) => {
+    const { projectId } = req.params.id;
+    const project = projects.find( ({ id }) => id === +projectId);
+
+    if (project)  {
+        res.render('project', { project });
+    } else {
+        res.sendStatus(404);
+    }    
+});
+
+/* Error handlers */
+
+// 404 errorhandler
+app.use((req, res, next) => {
+    const err = new Error();
+    err.status = 404;
+    err.message = 'The page you are trying to reach is not found';
+    console.log(err);
+});
+
+// Global error handler
+app.use((req, res, next) => {
+    
+});
+
+app.listen(3000, () => {
+    console.log('The app is running on localhost:3000');
+});
