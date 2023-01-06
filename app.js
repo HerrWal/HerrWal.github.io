2d07ba1b5062f13f41890e1126906ff2262cdeda
HerrWal <85037202+HerrWal@users.noreply.github.com>
2023-01-05 22:52:24 -0500

    jan 05

--- app.js
+++ app.js
@@ -24,38 +24,49 @@ app.get("/about", (req, res) => {
 
 // projects route
 app.get("/projects/:id", (req, res) => {
-  const  projectId  = req.params.id;
-  const project = projects.find(({ id }) => id === +projectId);
+  const projectId = req.params.id;
+  const project = projects.find(({ id }) => id === +projectId);  
+  res.render("project", { project });
+});
 
-  if (project) {
-    res.render("project", { project });
-  } else {
-    res.sendStatus(404);
-  }
+// error route
+app.get("/error", (req, res, next) => {
+  console.log('Error route called');
+  const err = new Error();
+  err.message = `500 error thrown`
+  err.status = 500;
+  res.next(err);
 });
 
 /* Error handlers */
 
 // 404 errorhandler
 app.use((req, res, next) => {
-  const err = new Error();
+  console.log('404 handler called');
+  const err = new Error("The page you are trying to reach is not found");
   err.status = 404;
-  err.message = "The page you are trying to reach is not found";
   console.log(err);
   next(err);
 });
 
 // Global error handler
-app.use((err, req, res, next) => {
-  // After the 404 handler in app.js add a global error handler that will deal with any server errors the app encounters. This handler should ensure that there is an err.status property and an err.message property if they don't already exist, and then log out the err object's message and status.
+// app.use((err, req, res, next) => {
+//   console.log('global Handler called');
+//   res.status(err.status || 500);
+//   res.send(err.message);
+//   console.log("Whoops, an error has ocurred!");
+//   console.error(err);
+// });
 
+app.use((err, req, res, next) => {
+  console.log('global Handler called');
   res.status(err.status || 500);
   res.send(err.message);
   console.log("Whoops, an error has ocurred!");
   console.error(err);
-  // Test your error handling by pointing the browser at a few undefined routes, like /noroute and /project/noroute, as well as temporarily throwing an intentional 500 error in one of your routes and then navigating to the page for that route.
 });
 
+// Server setup
 app.listen(3000, () => {
   console.log("The app is running on localhost:3000");
 });
